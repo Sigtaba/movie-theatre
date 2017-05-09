8e84f1f367d9783ca9901dc5fa65d2c0afef6991
Isabella Abatgis and Maria Del Castillo <maria@>
2017-05-09 16:10:57 -0700

    add ticket pricing

--- js/scripts.js
+++ js/scripts.js
@@ -6,22 +6,32 @@ function Ticket(movies, ages, times) {
   this.times = times;
 }
 
-Ticket.prototype.price = function() {
-  return this.movies + this.ages + this.times;
+Ticket.prototype.getPrice = function() {
+    var price = 0;
+  if (this.movies === "movie-premium") {
+    price += 10;
+  }
+  if (this.ages === "age-discount") {
+    price += 5;
+  }
+  if (this.times === "time-discount") {
+    price += 5;
+  }
+  return price;
 }
-
 
 //front-end logic
 
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
+
     var inputtedAge = $("input[name=age]:checked").val();
     var inputtedTime = $("input[name=time]:checked").val();
     var inputtedMovie = $("input[name=movie]:checked").val();
 
     var newTicket = new Ticket(inputtedMovie, inputtedAge, inputtedTime);
 
-    console.log(newTicket);
+    $("#output").text("Your price is: $" + newTicket.getPrice() + ".00");
 
     event.preventDefault();
   });
